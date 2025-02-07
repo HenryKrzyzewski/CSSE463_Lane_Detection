31e44fd0782f2487e19483274989f99201a6c488
Daniel Gaull <gaulldj@rose-hulman.edu>
2025-02-07 15:20:02 -0500

    Some more deeplab work, still errors out for now

--- deeplabExample.m
+++ deeplabExample.m
@@ -1,27 +1,25 @@
 function data = preprocessTrainingData(data, imageSize)
+% Resize the training image and associated pixel label image.
 data{1} = imresize(data{1}, imageSize);
 data{2} = imresize(data{2}, imageSize);
-end
 
-function data = preprocessPixelData(data, imageSize)
-data{1} = imresize(data{1}, imageSize);
-data{2} = imresize(data{2}, imageSize);
+% Convert grayscale input image into RGB for use with ResNet-18, which
+% requires RGB image input.
 data{1} = repmat(data{1},1,1,3);
 end
 
 %%
-
-imageDir = "./dataset/split/train";
+dataSetDir = fullfile(toolboxdir("vision"),"visiondata","triangleImages");
+imageDir = fullfile(dataSetDir,"trainingImages");
 imds = imageDatastore(imageDir);
-labelDir = "./dataset/split/train";
+labelDir = fullfile(dataSetDir, "trainingLabels");
 classNames = ["triangle","background"];
-labelIDs   = [1 0];
+labelIDs   = [255 0];
 pxds = pixelLabelDatastore(labelDir,classNames,labelIDs);
 imageSize = [256 256];
 numClasses = numel(classNames);
-imtds = transform(imds, @(data)preprocessTrainingData(data, imageSize));
-pxtds = transform(pxds, @(data)preprocessPixelData(data, imageSize));
-cds = combine(imtds,pxtds);
+cds = combine(imds,pxds);
+tds = transform(cds, @(data)preprocessTrainingData(data,imageSize));
 fprintf("Done\n");
 
 %%
@@ -30,4 +28,4 @@ opts = trainingOptions("sgdm",...
     MiniBatchSize=8,...
     MaxEpochs=3);
 
-net = trainnet(cds,net,"crossentropy",opts);
+net = trainnet(tds,net,"crossentropy",opts);
