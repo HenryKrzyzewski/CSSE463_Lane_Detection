7800874bece2bc1c93abbd3e8d066b6f6b15d060
Daniel Gaull <gaulldj@rose-hulman.edu>
2025-02-14 14:13:49 -0500

    Added more result displaying

--- deeplab.m
+++ deeplab.m
@@ -28,21 +28,55 @@ opts = trainingOptions("sgdm",...
 
 %%
 net = trainnet(tds,net,"crossentropy",opts);
-save('./deepnet.mat', net);
+
+%%
+save("./deepnet.mat", 'net');
 
 %%
 testImg = imread('dataset/split/test/000_Both_left_curve_0009.jpg');
 testImg = imresize(testImg, imageSize);
+trueMask = imread('dataset/split/test/000_Both_left_curve_0009.png');
+trueMask = logical(trueMask);
+trueMask = imresize(trueMask, imageSize);
 prediction = semanticseg(testImg, net);
 predictionMask = zeros(size(prediction));
-predictionMask(prediction == 'C2') = 1;
+predictionMask(prediction == 'C1') = 1;
 result = labeloverlay(testImg, prediction);
-%%
-figure;
-subplot(1, 3, 1);
-imshow(testImg);
-subplot(1, 3, 2);
-imshow(predictionMask);
-subplot(1, 3, 3);
-imshow(result);
 
+testImg2 = imread('dataset/split/test/000_Right_right_curve_3965.jpg');
+testImg2 = imresize(testImg2, imageSize);
+trueMask2 = imread('dataset/split/test/000_Right_right_curve_3965.png');
+trueMask2 = logical(trueMask2);
+trueMask2 = imresize(trueMask2, imageSize);
+prediction2 = semanticseg(testImg2, net);
+predictionMask2 = zeros(size(prediction2));
+predictionMask2(prediction2 == 'C1') = 1;
+result2 = labeloverlay(testImg2, prediction2);
+
+figure;
+
+subplot(2, 4, 1);
+imshow(testImg);
+title('Source Image');
+subplot(2, 4, 2);
+imshow(trueMask);
+title('True Mask');
+subplot(2, 4, 3);
+imshow(predictionMask);
+title('Prediction Mask');
+subplot(2, 4, 4);
+imshow(result);
+title('Overlay on Original');
+
+subplot(2, 4, 5);
+imshow(testImg2);
+title('Source Image');
+subplot(2, 4, 6);
+imshow(trueMask2);
+title('True Mask');
+subplot(2, 4, 7);
+imshow(predictionMask2);
+title('Prediction Mask');
+subplot(2, 4, 8);
+imshow(result2);
+title('Overlay on Original');
